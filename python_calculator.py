4dfef158a0f4ca79b7ec73bb0d414e6b8a0d5bc8
boryanaradeva02 <boryanaradeva02@gmail.com>
2026-09-23 17:28:33 +0300

    Add files via upload

---
+++ python_calculator.py
@@ -0,0 +1,21 @@
+print("Welcome to the VERY simple Python calculator:")
+
+operator = input("Enter an operator (+ - * /): ")
+num1 = float(input("Enter the first number :) "))
+num2 = float(input("Enter the second number :) "))
+
+
+if operator == '+':
+    result = num1 + num2
+    print(round(result, 2))
+elif operator == '-':
+    result = num1 - num2
+    print(round(result, 2))
+elif operator == '*':
+    result = num1 * num2
+    print(round(result, 2))
+elif operator == '/':
+    result = num1 / num2
+    print(round(result, 2))
+else:
+    print(f"{operator} valid operator")
